caff06cec2ab3a4047411f12440d432e942b3c99
Tio Muhamad Nur <outsider.kuningan52@gmail.com>
2026-01-15 15:53:01 +0700

    update: trial engine pdf generator baru

--- config/snappy.php
+++ config/snappy.php
@@ -52,7 +52,7 @@ return [
             'quiet' => true,
             'load-error-handling' => 'ignore',
             'load-media-error-handling' => 'ignore',
-            'dpi' => 10,
+            'dpi' => 30,
         ],
         'env'     => [],
     ],
